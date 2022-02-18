ed5179947617ad70149367a213a5feb88442feeb
mohamed-2018 <52450264+mohamed-2018@users.noreply.github.com>
2022-02-18 16:00:22 +0200

    adding new person for testing webhook comment

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(90);
+		p.setFirstName("Mohamed");
+		p.setLastName("Ali");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
